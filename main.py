69449f65233032f579332d2aeada614b480eaffe
Mooro869 <fedorovmatvey48@gmail.com>
2023-11-27 07:29:01 +0300

    Начало создания

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import aiogram
+import logging
+from random import randint
+from time import sleep
+
+import executor
+from aiogram import Bot, Dispatcher, types
+
+import config
+import keyboard
+
+bot = Bot(token=config.TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
