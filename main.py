be794d06c7382f0203b1065ca992f057c79b2d34
Mooro869 <fedorovmatvey48@gmail.com>
2023-11-27 18:44:22 +0300

    Пустой бот

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-import aiogram
 import logging
 from random import randint
 from time import sleep
 
-import executor
-from aiogram import Bot, Dispatcher, types  #При импорте executor здесь выводит ошиькуе
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
 import config
 import keyboard
 
 bot = Bot(token=config.TOKEN_API)
-dp = Dispatcher(bot) # ещё одна ошибка здесь 
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
-
+dp = Dispatcher(bot=bot)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
