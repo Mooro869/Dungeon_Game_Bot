375a078446a8b8e3d2f9b6f6a5ff30207a5f3a78
Mooro869 <fedorovmatvey48@gmail.com>
2023-11-27 19:53:43 +0300

    Немного написанные start и help

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from random import randint
 from time import sleep
 
@@ -11,6 +12,18 @@ import keyboard
 
 bot = Bot(token=config.TOKEN_API)
 dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer(text=config.START_TEXT)
+    await message.delete()
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(text=config.HELP_COMMANDS)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
